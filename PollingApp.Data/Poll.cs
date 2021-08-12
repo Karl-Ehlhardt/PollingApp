d5064db47ad96773c555bfb5181edaa775195324
jasondavis <jasondavis925@gmail.com>
2021-08-12 07:37:58 -0400

    add data_layerV0

---
+++ PollingApp.Data/Poll.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PollingApp.Data
+{
+    class Questions
+    {
+        [key]
+        public int Id { get; set; }
+
+        [required]
+        public int OwnerId { get; set; }
+
+        [required]
+        public string PollQuestion { get; set; }
+
+        [required]
+        public int PublishFlag { get; set; }
+
+        [required]
+        public int Response_multiFlag { get; set; }
+    }
+}
